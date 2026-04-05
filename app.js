9a739f04a1b9386821bdd91fef84120699ce4d16
prabeshbuilds <prabeshuser407@gmail.com>
2026-04-05 12:42:59 +0545

    some line added on the app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Hello from Node.js inside Docker!");
+console.log("This is a simple Node.js application running inside a Docker container.");
+
+// You can add more functionality here, such as a web server or database connection
